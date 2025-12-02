35ecdeb64994059aa31fc0f6c4b8b2c92ec7250c
Hemnarayan <hemnarayanyadav030@gmail.com>
2025-12-02 16:06:26 +0530

    delete old pic

--- src/assets/images/index.js
+++
@@ -1,6 +0,0 @@
-import Logo from './logo-main-.png';
-import FavIcon from './fav-icon.webp';
-import LogoFooter from './footer-logo-2.png';
-import Avatar from './admin/team/avatar.png';
-
-export { Logo, FavIcon, LogoFooter, Avatar }
